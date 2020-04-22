3bc30e22c0e1f77b1eaaaf1da3ac46f31d5385ed
Anas <anas.malik.yousef@gmail.com>
2020-04-22 18:13:42 +0300

    Added three components

--- app/src/main/java/com/example/ex1/MainActivity.java
+++ app/src/main/java/com/example/ex1/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText = findViewById(R.id.enteredText);
+        Button button = findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.textView);
+
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String enteredText = editText.getText().toString();
+               if (enteredText.equals(""))
+               {
+                   textView.setText("Enter Something Other Than Empty String");
+               }
+               else {
+                   textView.setText(enteredText);
+               }
+               editText.setText("");
+
+           }
+       });
     }
 }
